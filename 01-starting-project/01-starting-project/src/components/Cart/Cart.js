cd23986f57113ed89e546bb05364a9596fa304bd
voldemortC <monikarani62002@gmail.com>
2023-05-08 21:32:08 +0530

    component-approach

--- 01-starting-project/01-starting-project/src/components/Cart/Cart.js
+++ 01-starting-project/01-starting-project/src/components/Cart/Cart.js
@@ -1,15 +1,76 @@
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import Card from '../UI/Card';
+import { useEffect } from 'react';
 import classes from './Cart.module.css';
+import { uiActions } from '../../store/ui-slice';
 import CartItem from './CartItem';
 
+let isInitial = true;
+
 const Cart = (props) => {
+  const cartItems = useSelector((state) => state.cart.items);
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data!',
+        })
+      );
+      const response = await fetch(
+        'https://react-http-6b4a6.firebaseio.com/cart.json',
+      );
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+
+      console.log(response.json(), "dufhiwuefyhwfyhiwefyhlwey");
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully!',
+        })
+      );
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed!',
+        })
+      );
+    });
+  }, [cart, dispatch]);
+
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {cartItems.map((item) => (
+          <CartItem
+            key={item.id}
+            item={{
+              id: item.id,
+              title: item.name,
+              quantity: item.quantity,
+              total: item.totalPrice,
+              price: item.price,
+            }}
+          />
+        ))}
       </ul>
     </Card>
   );
