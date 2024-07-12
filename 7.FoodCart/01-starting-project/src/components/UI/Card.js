c62775361a35badc3ef493259eaf5b2939c81b6d
Monika <kulwinde5631@gmail.com>
2024-07-12 22:33:16 +0530

    making movie repo public

--- 7.FoodCart/01-starting-project/src/components/UI/Card.js
+++
@@ -1,9 +0,0 @@
-import "./Card.css";
-export default function Card(props){
-    const style = "card "+ props.className;
-    return(
-        <div className = {style}>
-            {props.children}
-        </div>
-    );
-}
